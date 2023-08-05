0c1797d384d0c5f61dbee63d6ac77570046d5673
Nadeem Ali <nadeemali001@gmail.com>
2023-08-05 20:54:15 +0530

    bounce the ball

---
+++ ball.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape('circle')
+        #self.shapesize(stretch_len=2, stretch_wid=2)
+        self.fillcolor('white')
+        self.color('white')
+        self.goto(0, 0)
+        self.move_x = 10
+        self.move_y = 10
+        self.penup()
+
+    def move_ball(self):
+        new_x = self.xcor() + self.move_x
+        new_y = self.ycor() + self.move_y
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.move_y *= -1
--- main.py
+++ main.py
@@ -1,2 +1,36 @@
 from turtle import Screen
+from paddle import Paddle
+from ball import Ball
+import time
 
+scn = Screen()
+l_pad = Paddle((350, 0))
+r_pad = Paddle((-350, 0))
+ball = Ball()
+
+
+scn.setup(width=800, height=600)
+scn.bgcolor('black')
+scn.title('Pong Game')
+scn.tracer(0)
+
+
+
+scn.listen()
+scn.onkey(l_pad.move_up, "Up")
+scn.onkey(l_pad.move_down, 'Down')
+scn.onkey(r_pad.move_up, "w")
+scn.onkey(r_pad.move_down, 's')
+
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    scn.update()
+    ball.move_ball()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+
+scn.exitonclick()
